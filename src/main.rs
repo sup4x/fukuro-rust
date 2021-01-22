e4ad17d1064d6be9ce8ba74772444a9275c5166b
Суп <charly.murdet@gmail.com>
2021-01-23 00:44:58 +0300

    auto env

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,18 @@ use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let node_env: String = env::var("NODE_ENV")
-        .unwrap_or_else(|_| "development".to_string())
-        .parse()
-        .expect("NODE_ENV must be a string");
-
     let port = env::var("PORT")
         .unwrap_or_else(|_| "8081".to_string())
         .parse()
         .expect("PORT must be a number");
-    let address = if node_env.eq("production") { "0.0.0.0" } else { "127.0.0.1" };
-
-    println!("Env is {:?}", node_env);
-    println!("Server is working on {:?} {:?}", address, port);
+    println!("Server is working on {:?}", port);
     let chat_server = Lobby::default().start(); //create and spin up a lobby
     HttpServer::new(
         move || App::new()
             .service(start_connection_route)
             .data(chat_server.clone())
     )
-        .bind((address, port))
+        .bind(("0.0.0.0", port))
         .expect("Can not bind to port")
         .run()
         .await
